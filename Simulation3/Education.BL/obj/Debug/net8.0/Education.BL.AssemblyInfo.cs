f62d8734ffc174a602f57039ae9c9141b446d39a
Aytacorucali <aytacho-ap215@code.edu.az>
2025-01-16 02:33:55 +0400

    test

--- Simulation3/Education.BL/obj/Debug/net8.0/Education.BL.AssemblyInfo.cs
+++ Simulation3/Education.BL/obj/Debug/net8.0/Education.BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Education.BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c29e8d4af601e0b01740cefa8e04dfebb7b5dd64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Education.BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Education.BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
