73354f618d0fbf41c44c992d10d910e9b03e6d4a
Aytacorucali <aytacho-ap215@code.edu.az>
2025-01-15 11:17:29 +0400

    test2

--- Simulation2.MVC/Plumberz.MVC/obj/Debug/net8.0/Plumberz.MVC.AssemblyInfo.cs
+++ Simulation2.MVC/Plumberz.MVC/obj/Debug/net8.0/Plumberz.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Plumberz.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9587796e1d2add6118421fafac90908b22c54721")]
 [assembly: System.Reflection.AssemblyProductAttribute("Plumberz.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Plumberz.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
